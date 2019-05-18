6e48ca44dd539bb1a9a05a1a447454bb3dcc0f80
Willy <wilsonoh@outlook.es>
2019-05-18 18:04:49 -0400

    Codigo de seguidor de linea

--- SeguidorLinea/SeguidorLinea.ino
+++ SeguidorLinea/SeguidorLinea.ino
@@ -10,12 +10,10 @@
 #define B1 10
 #define B2 11
 
-#define velocidad 255
+#define velocidad 200
 
 #define lineaMax 1023
 #define lineaMin 200
-
-
 
 void setup() {
   Serial.begin(9600);
@@ -29,34 +27,28 @@ void setup() {
   pinMode(A2,OUTPUT);
   pinMode(B1,OUTPUT);
   pinMode(B2,OUTPUT);
-
 }
-
+//Funciones de funcinamiento del robot
 void frente(){
     Serial.println("Frente");
   analogWrite(A1,velocidad);
   analogWrite(B1,velocidad);
   analogWrite(A2,0);
-  analogWrite(B2,0);
-  
-  }
-  
+  analogWrite(B2,0);  
+  }  
   void derechaF(){
-    Serial.println("Derecha Suave");
+    Serial.println("Derecha Fuerte");
   analogWrite(A1,velocidad);
   analogWrite(B1,velocidad/3);
   analogWrite(A2,0);
-  analogWrite(B2,0);
-  
+  analogWrite(B2,0);  
   }
-
     void izquierdaF(){
     Serial.println("Izquierda Fuerte");
   analogWrite(A1,velocidad/3);
   analogWrite(B1,velocidad);
   analogWrite(A2,0);
-  analogWrite(B2,0);
-  
+  analogWrite(B2,0);  
   }
 
    void derechaS(){
@@ -82,14 +74,14 @@ void frente(){
     int d2=analogRead(D2);  //Sensor medio derecha
     int d3=analogRead(D3);  //Sensor medio izquierda
     int d4=analogRead(D4);  //Sensor izquierda
-  /*Serial.print("\t");
+  Serial.print("\t");
   Serial.print(d1);
   Serial.print("\t");
   Serial.print(d2);
   Serial.print("\t");
   Serial.print(d3);
   Serial.print("\t");
-  Serial.println(d4);*/
+  Serial.println(d4);
   
     if(d1>lineaMin && d2<lineaMin&& d3<lineaMin&& d4<lineaMin){
       return 1;
@@ -111,14 +103,10 @@ if(d1<lineaMin && d2>lineaMin&& d3>lineaMin&& d4<lineaMin){
       if(d1<lineaMin && d2>lineaMin&& d3<lineaMin&& d4<lineaMin){
       return 5;
       }
-      
-
-      
-    
     }
 
 void loop() {
-
+//sensor();
 switch(sensor())
 {
     case 1:
@@ -142,5 +130,5 @@ switch(sensor())
     break;  
   }
   
-  //delay(100);
+//delay(100);
 }
